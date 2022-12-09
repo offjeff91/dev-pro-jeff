785c03ac72f34d1f9d45366f3456aad3bba19cbc
Jefferson Fernandes <offjeff91@gmail.com>
2022-12-09 20:11:53 -0300

    refactor: extract input parsing into InputParser

--- code.rb
+++ code.rb
@@ -1,4 +1,29 @@
 def solution(s)
-  format = [ :image_file, :city, :date]
-  data = s.split("\n").map { |line| line.split(', ').each_with_index.map { |value, index| { format[index] => value } }.reduce(:merge) }
+  # format = [ :image_file, :city, :date]
+  # data = s.split("\n").map { |line| line.split(', ').each_with_index.map { |value, index| { format[index] => value } }.reduce(:merge) }
+  data = ListParser.new(s).parse
+  # write response for data
+end
+
+class ListParser
+  FORMAT = [ :image_file, :city, :date]
+  def initialize input
+    @input = input
+  end
+  def parse
+    lines.map { |line| parse_line(line) }
+  end
+  protected
+  def lines
+    @input.split("\n")
+  end
+  def parse_line(line)
+    values(line).map { |value, index| build_hash(value, index) }.reduce(:merge)
+  end
+  def values(line)
+    line.split(', ').each_with_index
+  end
+  def build_hash(value, index)
+    { ListParser::FORMAT[index] => value }
+  end
 end
--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-"photo.jpg, Krakow, 2013-09-05 14:08:15
+s = "photo.jpg, Krakow, 2013-09-05 14:08:15
 Mike.png, London, 2015-06-20 15:13:22
 myFriends.png, Krakow, 2013-09-05 14:07:13
 Eiffel.jpg, Florianopolis, 2015-07-23 08:03:02
@@ -13,6 +13,8 @@ d.jpg, Krakow, 2016-01-02 15:15:01
 e.png, Krakow, 2016-01-02 09:49:09
 f.png, Krakow, 2016-01-02 10:55:32
 g.jpg, Krakow, 2016-02-29 22:13:11"
+
+solution(s)
 
 # Krakow02.jpg
 # London1.png
