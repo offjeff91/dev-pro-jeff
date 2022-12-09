bae85b0f5d9424296aa8bbba30f0855e4489950f
Jefferson Fernandes <offjeff91@gmail.com>
2022-12-09 19:26:18 -0300

    feat: read data as an array of photos

--- code.rb
+++ code.rb
@@ -1,4 +1,4 @@
 def solution(s)
   format = [ :image_file, :city, :date]
-  array = s.split "\n"
+  data = s.split("\n").map { |line| line.split(', ').each_with_index.map { |value, index| { format[index] => value } }.reduce(:merge) }
 end
